23861ceac7beedbb38e7446ed0b50c552d20ed0d
Mendes, Eric Silva <eric.silva.mendes@accenture.com>
2025-07-19 22:41:44 -0300

    feat: config prisma orm

---
+++ prisma/migrations/20250720013738_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "PaymentProviders" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "maxRetry" INTEGER NOT NULL,
+    "timeout" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "detailid" TEXT NOT NULL,
+    CONSTRAINT "PaymentProviders_detailid_fkey" FOREIGN KEY ("detailid") REFERENCES "ProviderDetail" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ProviderDetail" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "url" TEXT NOT NULL,
+    "requireAuthentication" BOOLEAN NOT NULL DEFAULT false,
+    "authenticationId" TEXT NOT NULL,
+    CONSTRAINT "ProviderDetail_authenticationId_fkey" FOREIGN KEY ("authenticationId") REFERENCES "ProviderAuthentication" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ProviderAuthentication" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "url" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'NONE',
+    "username" TEXT,
+    "password" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PaymentProviders_name_key" ON "PaymentProviders"("name");
