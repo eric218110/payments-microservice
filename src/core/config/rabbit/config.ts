4079e1c8d406df31a915e4541778e77f90d32113
Mendes, Eric Silva <eric.silva.mendes@accenture.com>
2025-07-27 09:43:11 -0300

    feat: config rabbit for listener queue processPaymentOut

--- src/core/config/rabbit/config.ts
+++ src/core/config/rabbit/config.ts
@@ -1,14 +1,18 @@
 import { INestApplication } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { tokensQueue } from 'src/domain/shared/tokens/tokens_queue';
 
 export const setupRabbit = (app: INestApplication<any>) => {
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.RMQ,
-  //   options: {
-  //     urls: [process.env.RABBIT_CONNECTION_STRING],
-  //     queue: tokensQueue.processPayment,
-  //     queueOptions: { durable: true },
-  //   },
-  // });
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBIT_CONNECTION_STRING],
+      queue: tokensQueue.processPaymentOut,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
 
   return app;
 };
